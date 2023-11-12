1ef7ddb9cc28a3c67d3b2720bc7159b02dfc94bb
RyouKinoshita <ryoukinoshita@gmail.com>
2023-11-12 13:19:27 +0800

    Get user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const upload = require('../utils/multer')
+const upload = require("../utils/multer");
 
-const { registerUser, loginUser,logout,forgotPassword,resetPassword} = require('../controllers/authController');
+const {
+  registerUser,
+  loginUser,
+  logout,
+  forgotPassword,
+  resetPassword,
+  getUserProfile,
+} = require("../controllers/authController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
+router.post("/register", upload.single("avatar"), registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
 
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
 
-router.post('/register',upload.single("avatar"),registerUser);
-router.post('/login', loginUser);
-router.get('/logout',logout);
-
-router.post('/password/forgot',forgotPassword);
-router.put('/password/reset/:token',resetPassword);
-
+router.get("/me", isAuthenticatedUser, getUserProfile);
 
 module.exports = router;
