5fb851409251376ffad79b98e62eb730ed7adc9f
ChanColossus <christianbenedict.quintero@tup.edu.ph>
2023-11-12 15:23:15 +0800

    Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './Components/Layout/Header'
 import Footer from './Components/Layout/Footer'
 import Home from './Components/Home'
+import Login from './Components/User/Login'
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
 function App() {
 
   return (
@@ -14,7 +18,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} exact="true" />
-          
+          <Route path="/login" element={<Login />} exact="true" />
         </Routes>
         <Footer />
       </Router>
--- frontend/src/Components/Layout/Footer.js
+++ frontend/src/Components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const Footer = () => {
     return (
         <Fragment>
-            <footer className="py-1">
+            <footer className="footer">
                 <p className="text-center mt-1" style={{ color: 'yellow', backgroundColor: 'black', padding: '30px' }}>
                     Myrmidons - 2022-2023, All Rights Reserved
                 </p>
