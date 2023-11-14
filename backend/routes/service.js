986fc7f7053e0b7d71fa6018cf2fac4813efe5f2
ChanColossus <christianbenedict.quintero@tup.edu.ph>
2023-11-14 22:44:08 +0800

    isauthsingleservice

--- backend/routes/service.js
+++ backend/routes/service.js
@@ -5,6 +5,6 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.post('/service/new', newService);
 router.get('/service', getServices);
-router.get('/service/:id',getSingleService);
+router.get('/service/:id',isAuthenticatedUser,getSingleService);
 router.route('/admin/service/:id').put(updateService).delete(deleteService);
 module.exports = router;
