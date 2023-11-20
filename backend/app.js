03077fbfe902ad89645b5fd14d52a41e4d803ef6
RyouKinoshita <ryoukinoshita@gmail.com>
2023-11-20 14:22:23 +0800

    get admin services

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
- const services = require('./routes/service');
-const auth = require('./routes/auth');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const services = require("./routes/service");
+const auth = require("./routes/auth");
 // const order = require('./routes/order');
 
-app.use(cors())
-app.use(express.json({limit:'50mb'}));
-app.use(express.urlencoded({limit: "50mb", extended: true }));
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(express.json());
-app.use('/api/v1', services);
+// app.use(express.json());
+app.use("/api/v1", services);
 
-app.use('/api/v1', auth);
+app.use("/api/v1", auth);
 
-
-
-
-module.exports = app
+module.exports = app;
--- backend/routes/service.js
+++ backend/routes/service.js
@@ -6,11 +6,19 @@ const {
   updateService,
   deleteService,
   getSingleService,
+  getAdminServices,
 } = require("../controllers/serviceController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.post("/service/new", newService);
 router.get("/service", getServices);
+
 router.get("/service/:id", getSingleService);
+router.get(
+  "/admin/service",
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  getAdminServices
+);
 router.route("/admin/service/:id").put(updateService).delete(deleteService);
 module.exports = router;
