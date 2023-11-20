e549b8290f12ee8b25a6055e511f3eb2db277b17
ChanColossus <christianbenedict.quintero@tup.edu.ph>
2023-11-20 12:35:56 +0800

    middleware

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,11 @@ app.use(cors())
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(cookieParser());
-// app.use(express.json());
+app.use(express.json());
 app.use('/api/v1', services);
-// app.use('/api/v1', products);
+
 app.use('/api/v1', auth);
-// app.use('/api/v1', order);
+
 
 
 
