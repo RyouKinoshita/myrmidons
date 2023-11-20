9c70edae254e9b98a810077399b6abe062951aa3
RyouKinoshita <ryoukinoshita@gmail.com>
2023-11-20 13:12:57 +0800

    loader and protected route

--- backend/routes/service.js
+++ backend/routes/service.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {newService,getServices,updateService,deleteService,getSingleService} = require('../controllers/serviceController');
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+const {
+  newService,
+  getServices,
+  updateService,
+  deleteService,
+  getSingleService,
+} = require("../controllers/serviceController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
-router.post('/service/new', newService);
-router.get('/service', getServices);
-router.get('/service/:id',getSingleService);
-router.route('/admin/service/:id').put(updateService).delete(deleteService);
+router.post("/service/new", newService);
+router.get("/service", getServices);
+router.get("/service/:id", getSingleService);
+router.route("/admin/service/:id").put(updateService).delete(deleteService);
 module.exports = router;
