dbf9519cd2e0f811688c5630b7e13e6631abf15c
RyouKinoshita <ryoukinoshita@gmail.com>
2023-11-27 02:55:57 +0800

    register form validation

--- frontend/src/Components/User/Register.js
+++ frontend/src/Components/User/Register.js
@@ -2,23 +2,16 @@ import React, { Fragment, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Metadata from "../Layout/Metadata";
 import axios from "axios";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Register = () => {
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-
-  const { name, email, password } = user;
-
   const [avatar, setAvatar] = useState("");
   const [avatarPreview, setAvatarPreview] = useState(
     "/images/default_avatar.jpg"
   );
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState("");
-  // const [loading, setLoading] = useState(true)
 
   let navigate = useNavigate();
   useEffect(() => {
@@ -30,18 +23,6 @@ const Register = () => {
       setError();
     }
   }, [error.isAuthenticated, navigate]);
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.set("name", name);
-    formData.set("email", email);
-    formData.set("password", password);
-    formData.set("avatar", avatar);
-
-    register(formData);
-  };
 
   const onChange = (e) => {
     if (e.target.name === "avatar") {
@@ -55,8 +36,6 @@ const Register = () => {
       };
 
       reader.readAsDataURL(e.target.files[0]);
-    } else {
-      setUser({ ...user, [e.target.name]: e.target.value });
     }
   };
 
@@ -75,17 +54,26 @@ const Register = () => {
       );
       console.log(data.user);
       setIsAuthenticated(true);
-      // setLoading(false)
-      setUser(data.user);
       navigate("/");
     } catch (error) {
       setIsAuthenticated(false);
-      // setLoading(false)
-      setUser(null);
       setError(error.response.data.message);
       console.log(error.response.data.message);
     }
   };
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string().email("Invalid email").required("Password is required"),
+    password: Yup.string()
+      .min(8, "Password is too short - should be 8 chars minimum.")
+      .required("Password is required"),
+    avatar: Yup.mixed()
+      .required("Image is required")
+      .test("fileSize", "Up to 10mb of file size only", (value) => {
+        return value && value.size <= 1024 * 1024 * 10; // 10 MB
+      }),
+  });
 
   return (
     <Fragment>
@@ -93,86 +81,116 @@ const Register = () => {
 
       <div className="row wrapper">
         <div className="col-10 col-lg-5">
-          <form
-            className="shadow-lg"
-            onSubmit={submitHandler}
-            encType="multipart/form-data"
+          <Formik
+            initialValues={{ name: "", email: "", password: "", avatar: "" }}
+            validationSchema={validationSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              setSubmitting(true);
+
+              const formData = new FormData();
+              formData.set("name", values.name);
+              formData.set("email", values.email);
+              formData.set("password", values.password);
+              formData.set("avatar", avatar);
+
+              register(formData);
+            }}
           >
-            <h1 className="mb-3">Register</h1>
+            {({ setFieldValue, errors, touched }) => (
+              <Form className="shadow-lg" encType="multipart/form-data">
+                <h1 className="mb-3">Register</h1>
 
-            <div className="form-group">
-              <label htmlFor="email_field">Name</label>
-              <input
-                type="name"
-                id="name_field"
-                className="form-control"
-                name="name"
-                value={name}
-                onChange={onChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="email_field">Email</label>
-              <input
-                type="email"
-                id="email_field"
-                className="form-control"
-                name="email"
-                value={email}
-                onChange={onChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="password_field">Password</label>
-              <input
-                type="password"
-                id="password_field"
-                className="form-control"
-                name="password"
-                value={password}
-                onChange={onChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="avatar_upload">Avatar</label>
-              <div className="d-flex align-items-center">
-                <div>
-                  <figure className="avatar mr-3 item-rtl">
-                    <img
-                      src={avatarPreview}
-                      className="rounded-circle"
-                      alt="Avatar Preview"
-                    />
-                  </figure>
-                </div>
-                <div className="custom-file">
-                  <input
-                    type="file"
-                    name="avatar"
-                    className="custom-file-input"
-                    id="customFile"
-                    accept="images/*"
-                    onChange={onChange}
+                <div className="form-group">
+                  <label htmlFor="name_field">Name</label>
+                  <Field
+                    type="text"
+                    id="name_field"
+                    className="form-control"
+                    name="name"
                   />
-                  <label className="custom-file-label" htmlFor="customFile">
-                    Choose Avatar
-                  </label>
+                  {errors.name && touched.name ? (
+                    <div className="invalid-feedback d-block">
+                      {errors.name}
+                    </div>
+                  ) : null}
                 </div>
-              </div>
-            </div>
 
-            <button
-              id="register_button"
-              type="submit"
-              className="btn btn-block py-3"
-              // disabled={loading ? false : true}
-            >
-              REGISTER
-            </button>
-          </form>
+                <div className="form-group">
+                  <label htmlFor="email_field">Email</label>
+                  <Field
+                    type="email"
+                    id="email_field"
+                    className="form-control"
+                    name="email"
+                  />
+                  {errors.email && touched.email ? (
+                    <div className="invalid-feedback d-block">
+                      {errors.email}
+                    </div>
+                  ) : null}
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password_field">Password</label>
+                  <Field
+                    type="password"
+                    id="password_field"
+                    className="form-control"
+                    name="password"
+                  />
+                  {errors.password && touched.password ? (
+                    <div className="invalid-feedback d-block">
+                      {errors.password}
+                    </div>
+                  ) : null}
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="avatar_upload">Avatar</label>
+                  <div className="d-flex align-items-center">
+                    <div>
+                      <figure className="avatar mr-3 item-rtl">
+                        <img
+                          src={avatarPreview}
+                          className="rounded-circle"
+                          alt="Avatar Preview"
+                        />
+                      </figure>
+                    </div>
+                    <div className="custom-file">
+                      <input
+                        type="file"
+                        name="avatar"
+                        className="custom-file-input"
+                        id="customFile"
+                        accept="images/*"
+                        onChange={(event) => {
+                          setFieldValue("avatar", event.currentTarget.files[0]);
+                          onChange(event);
+                        }}
+                      />
+                      <label className="custom-file-label" htmlFor="customFile">
+                        Choose Avatar
+                      </label>
+                    </div>
+                  </div>
+                  {errors.avatar && touched.avatar ? (
+                    <div className="invalid-feedback d-block">
+                      {errors.avatar}
+                    </div>
+                  ) : null}
+                </div>
+
+                <button
+                  id="register_button"
+                  type="submit"
+                  className="btn btn-block py-3"
+                >
+                  REGISTER
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </Fragment>
