eab47b7d4286713d7ea37afa5f938b16f7348656
ChanColossus <christianbenedict.quintero@tup.edu.ph>
2023-11-27 06:56:06 +0800

    Working frontend (missing formik of updateprof and register)

--- frontend/src/Components/User/Register.js
+++ frontend/src/Components/User/Register.js
@@ -2,16 +2,23 @@ import React, { Fragment, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Metadata from "../Layout/Metadata";
 import axios from "axios";
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import * as Yup from "yup";
 
 const Register = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = user;
+
   const [avatar, setAvatar] = useState("");
   const [avatarPreview, setAvatarPreview] = useState(
     "/images/default_avatar.jpg"
   );
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState("");
+  // const [loading, setLoading] = useState(true)
 
   let navigate = useNavigate();
   useEffect(() => {
@@ -23,6 +30,18 @@ const Register = () => {
       setError();
     }
   }, [error.isAuthenticated, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.set("name", name);
+    formData.set("email", email);
+    formData.set("password", password);
+    formData.set("avatar", avatar);
+
+    register(formData);
+  };
 
   const onChange = (e) => {
     if (e.target.name === "avatar") {
@@ -36,6 +55,8 @@ const Register = () => {
       };
 
       reader.readAsDataURL(e.target.files[0]);
+    } else {
+      setUser({ ...user, [e.target.name]: e.target.value });
     }
   };
 
@@ -54,53 +75,66 @@ const Register = () => {
       );
       console.log(data.user);
       setIsAuthenticated(true);
+      // setLoading(false)
+      setUser(data.user);
       navigate("/");
     } catch (error) {
       setIsAuthenticated(false);
+      // setLoading(false)
+      setUser(null);
       setError(error.response.data.message);
       console.log(error.response.data.message);
     }
   };
 
-  const validationSchema = Yup.object().shape({
-    name: Yup.string().required("Name is required"),
-    email: Yup.string().email("Invalid email").required("Password is required"),
-    password: Yup.string()
-      .min(8, "Password is too short - should be 8 chars minimum.")
-      .required("Password is required"),
-    avatar: Yup.mixed()
-      .required("Image is required")
-      .test("fileSize", "Up to 10mb of file size only", (value) => {
-        return value && value.size <= 1024 * 1024 * 10; // 10 MB
-      }),
-  });
-
   return (
     <Fragment>
-  <Metadata title={"Register User"} />
+      <Metadata title={"Register User"} />
 
-  <div className="row wrapper">
-    <div className="col-10 col-lg-5">
-      <Formik
-        initialValues={{ name: "", email: "", password: "", avatar: "" }}
-        validationSchema={validationSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          setSubmitting(true);
-
-          const formData = new FormData();
-          formData.set("name", values.name);
-          formData.set("email", values.email);
-          formData.set("password", values.password);
-          formData.set("avatar", avatar);
-
-          register(formData);
-        }}
-      >
-        {({ setFieldValue, errors, touched }) => (
-          <Form className="shadow-lg" encType="multipart/form-data">
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form
+            className="shadow-lg"
+            onSubmit={submitHandler}
+            encType="multipart/form-data"
+          >
             <h1 className="mb-3">Register</h1>
 
-            {/* ... other form fields ... */}
+            <div className="form-group">
+              <label htmlFor="email_field">Name</label>
+              <input
+                type="name"
+                id="name_field"
+                className="form-control"
+                name="name"
+                value={name}
+                onChange={onChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email_field">Email</label>
+              <input
+                type="email"
+                id="email_field"
+                className="form-control"
+                name="email"
+                value={email}
+                onChange={onChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password_field">Password</label>
+              <input
+                type="password"
+                id="password_field"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={onChange}
+              />
+            </div>
 
             <div className="form-group">
               <label htmlFor="avatar_upload">Avatar</label>
@@ -121,40 +155,28 @@ const Register = () => {
                     className="custom-file-input"
                     id="customFile"
                     accept="images/*"
-                    onChange={(event) => {
-                      setFieldValue(
-                        "avatar",
-                        event.currentTarget.files[0]
-                      );
-                      onChange(event);
-                    }}
+                    onChange={onChange}
                   />
                   <label className="custom-file-label" htmlFor="customFile">
                     Choose Avatar
                   </label>
                 </div>
               </div>
-              {errors.avatar && touched.avatar ? (
-                <div className="invalid-feedback d-block">
-                  {errors.avatar}
-                </div>
-              ) : null}
             </div>
 
-            {/* Corrected placement of the button */}
             <button
-              id="loginsbut" // Ensure this matches the ID in your JavaScript code
+              id="register_button"
               type="submit"
-              className="buttonforLogin"
+              className="btn btn-block py-3"
               // disabled={loading ? false : true}
             >
               REGISTER
             </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  </div>
-</Fragment>
-)}
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
 export default Register;
