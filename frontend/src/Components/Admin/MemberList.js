d8bea67a5c18b4dce6ccaec5f929bd918fbc2a9b
RyouKinoshita <ryoukinoshita@gmail.com>
2023-11-27 10:48:08 +0800

    dashboard modifications

--- frontend/src/Components/Admin/MemberList.js
+++ frontend/src/Components/Admin/MemberList.js
@@ -10,169 +10,172 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const MemberList = () => {
-    const [members, setMembers] = useState([]);
-    const [error, setError] = useState("");
-    const [deleteError, setDeleteError] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [isDeleted, setIsDeleted] = useState(false);
-  
-    let navigate = useNavigate();
-    const getAdminMember = async () => {
-      try {
-        const config = {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: `Bearer ${getToken()}`,
-          },
-        };
-  
-        const { data } = await axios.get(
-          `http://localhost:4001/api/v1/admin/MemberList`,
-          config
-        );
-        console.log(data.team);
-        setMembers(data.team);
-        setLoading(false);
-      } catch (error) {
-        setError(error.response.data.message);
-      }
-    };
-    useEffect(() => {
-      getAdminMember();
-  
-      if (error) {
-        toast.error(error, {
-          position: toast.POSITION.BOTTOM_RIGHT,
-        });
-      }
-  
-      if (deleteError) {
-        toast.error(deleteError, {
-          position: toast.POSITION.BOTTOM_RIGHT,
-        });
-      }
-  
-      if (isDeleted) {
-        toast.success("Member deleted successfully", {
-          position: toast.POSITION.BOTTOM_RIGHT,
-        });
-        navigate("/admin/MemberList");
-      }
-    }, [error, deleteError, isDeleted]);
-  
-    const deleteMember= async (id) => {
-      try {
-        const config = {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: `Bearer ${getToken()}`,
-          },
-        };
-        const { data } = await axios.delete(
-          `http://localhost:4001/api/v1/admin/Memberlist/${id}`,
-          config
-        );
-  
-        setIsDeleted(data.success);
-        setLoading(false);
-      } catch (error) {
-        setDeleteError(error.response.data.message);
-      }
-    };
-  
-    const membersList = () => {
-      const data = {
-        columns: [
-          {
-            label: "ID",
-            field: "id",
-            sort: "asc",
-          },
-          {
-            label: "Name",
-            field: "name",
-            sort: "asc",
-          },
-          {
-            label: "Position",
-            field: "position",
-            sort: "asc",
-          },
-          {
-            label: "Description",
-            field: "description",
-            sort: "asc",
-          },
-          {
-            label: "Actions",
-            field: "actions",
-          },
-        ],
-        rows: [],
+  const [members, setMembers] = useState([]);
+  const [error, setError] = useState("");
+  const [deleteError, setDeleteError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  let navigate = useNavigate();
+  const getAdminMember = async () => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${getToken()}`,
+        },
       };
-  
-      members.forEach((members) => {
-        data.rows.push({
-          id: members._id,
-          name: members.name,
-          position: members.position,
-          description: members.description,
-          actions :
-             (
-            <Fragment>
+
+      const { data } = await axios.get(
+        `http://localhost:4001/api/v1/admin/MemberList`,
+        config
+      );
+      console.log(data.team);
+      setMembers(data.team);
+      setLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+  useEffect(() => {
+    getAdminMember();
+
+    if (error) {
+      toast.error(error, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+
+    if (deleteError) {
+      toast.error(deleteError, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+
+    if (isDeleted) {
+      toast.success("Member deleted successfully", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      navigate("/admin/MemberList");
+    }
+  }, [error, deleteError, isDeleted]);
+
+  const deleteMember = async (id) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${getToken()}`,
+        },
+      };
+      const { data } = await axios.delete(
+        `http://localhost:4001/api/v1/admin/Memberlist/${id}`,
+        config
+      );
+
+      setIsDeleted(data.success);
+      setLoading(false);
+    } catch (error) {
+      setDeleteError(error.response.data.message);
+    }
+  };
+
+  const membersList = () => {
+    const data = {
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          sort: "asc",
+        },
+        {
+          label: "Name",
+          field: "name",
+          sort: "asc",
+        },
+        {
+          label: "Position",
+          field: "position",
+          sort: "asc",
+        },
+        {
+          label: "Description",
+          field: "description",
+          sort: "asc",
+        },
+        {
+          label: "Actions",
+          field: "actions",
+        },
+      ],
+      rows: [],
+    };
+
+    members.forEach((members) => {
+      data.rows.push({
+        id: members._id,
+        name: members.name,
+        position: members.position,
+        description: members.description,
+        actions: (
+          <Fragment>
+            <div className="button-container">
               <Link
                 to={`/admin/UpdateMember/${members._id}`}
                 className="btn btn-primary py-1 px-2"
+                title="Edit Member"
               >
                 <i className="fa fa-pencil"></i>
               </Link>
               <button
                 className="btn btn-danger py-1 px-2 ml-2"
+                title="Delete Member"
                 onClick={() => deleteMemberHandler(members._id)}
               >
                 <i className="fa fa-trash"></i>
               </button>
-            </Fragment>
-          ),
-        });
+            </div>
+          </Fragment>
+        ),
       });
-  
-      return data;
-    };
-  
-    const deleteMemberHandler = (id) => {
-      deleteMember(id);
-    };
-  
-    return (
-      <Fragment>
-        <MetaData title={"All Members"} />
-        <div className="row">
-          <div className="col-12 col-md-2">
-            <Sidebar />
-          </div>
-  
-          <div className="col-12 col-md-10">
-            <Fragment>
-              <h1 className="my-5">All Members</h1>
-  
-              {loading ? (
-                <Loader />
-              ) : (
-                <MDBDataTable
-                  data={membersList()}
-                  className="px-3"
-                  bordered
-                  striped
-                  hover
-                  style={{ color: "white", fontWeight: "bold"}}
-                />
-              )}
-            </Fragment>
-          </div>
-        </div>
-      </Fragment>
-    );
+    });
+
+    return data;
   };
-  
-  export default MemberList;
+
+  const deleteMemberHandler = (id) => {
+    deleteMember(id);
+  };
+
+  return (
+    <Fragment>
+      <MetaData title={"All Members"} />
+      <div className="row">
+        <div className="col-12 col-md-2">
+          <Sidebar />
+        </div>
+
+        <div className="col-12 col-md-10">
+          <Fragment>
+            <h1 className="my-5">All Members</h1>
+
+            {loading ? (
+              <Loader />
+            ) : (
+              <MDBDataTable
+                data={membersList()}
+                className="px-3"
+                bordered
+                striped
+                hover
+                style={{ color: "white", fontWeight: "bold" }}
+              />
+            )}
+          </Fragment>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default MemberList;
--- frontend/src/Components/Admin/ProjectList.js
+++ frontend/src/Components/Admin/ProjectList.js
@@ -11,14 +11,14 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const ProjectList = () => {
-    const [projects, setProjects] = useState([]);
-    const [error, setError] = useState("");
-    const [deleteError, setDeleteError] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [isDeleted, setIsDeleted] = useState(false);
-        
-    let navigate = useNavigate();
-    const getAdminProjects = async () => {
+  const [projects, setProjects] = useState([]);
+  const [error, setError] = useState("");
+  const [deleteError, setDeleteError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  let navigate = useNavigate();
+  const getAdminProjects = async () => {
     try {
       const config = {
         headers: {
@@ -41,142 +41,150 @@ const ProjectList = () => {
   useEffect(() => {
     getAdminProjects();
     if (error) {
-        toast.error(error, {
-          position: toast.POSITION.BOTTOM_RIGHT,
-        });
-      }
-  
-      if (deleteError) {
-        toast.error(deleteError, {
-          position: toast.POSITION.BOTTOM_RIGHT,
-        });
-      }
-  
-      if (isDeleted) {
-        toast.success("Project deleted successfully", {
-          position: toast.POSITION.BOTTOM_RIGHT,
-        });
-        navigate("/admin/portfolio");
-      }
-    }, [error, deleteError, isDeleted]);
-    const deleteProject = async (id) => {
-        try {
-          const config = {
-            headers: {
-              "Content-Type": "multipart/form-data",
-              Authorization: `Bearer ${getToken()}`,
-            },
-          };
-          const { data } = await axios.delete(
-            `http://localhost:4001/api/v1/admin/portfolio/${id}`,
-            config
-          );
-    
-          setIsDeleted(data.success);
-          setLoading(false);
-        } catch (error) {
-          setDeleteError(error.response.data.message);
-        }
+      toast.error(error, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+
+    if (deleteError) {
+      toast.error(deleteError, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+
+    if (isDeleted) {
+      toast.success("Project deleted successfully", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      navigate("/admin/portfolio");
+    }
+  }, [error, deleteError, isDeleted]);
+  const deleteProject = async (id) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${getToken()}`,
+        },
       };
-    
-      const projectsList = () => {
-        const data = {
-            columns: [
-                {
-                    label: "ID",
-                    field: "id",
-                    sort: "asc",
-                },
-                {
-                    label: "Name",
-                    field: "name",
-                    sort: "asc",
-                },
-                {
-                    label: "Location",
-                    field: "location",
-                    sort: "asc",
-                },
-                {
-                    label: "Date",
-                    field: "date",
-                    sort: "asc",
-                },
-                {
-                    label: "Actions",
-                    field: "actions",
-                },
-            ],
-            rows: [],
-        };
-    
-        projects.forEach((project) => {
-            // Format the date to display only the date part
-            const parsedDate = new Date(project.date);
-            const year = parsedDate.getFullYear();
-            const month = parsedDate.getMonth() + 1; // Month is zero-indexed, so add 1
-            const day = parsedDate.getDate();
-            const formattedDate = `${year}-${month < 10 ? "0" : ""}${month}-${day < 10 ? "0" : ""}${day}`;
-    
-            data.rows.push({
-                id: project._id,
-                name: project.name,
-                location: project.location,
-                date: formattedDate,
-                actions: (
-                    <Fragment>
-                        <Link
-                            to={`/admin/portfolio/${project._id}`}
-                            className="btn btn-primary py-1 px-2"
-                        >
-                            <i className="fa fa-pencil"></i>
-                        </Link>
-                        <button
-                            className="btn btn-danger py-1 px-2 ml-2"
-                            onClick={() => deleteServiceHandler(project._id)}
-                        >
-                            <i className="fa fa-trash"></i>
-                        </button>
-                    </Fragment>
-                ),
-            });
-        });
-    
-        return data;
+      const { data } = await axios.delete(
+        `http://localhost:4001/api/v1/admin/portfolio/${id}`,
+        config
+      );
+
+      setIsDeleted(data.success);
+      setLoading(false);
+    } catch (error) {
+      setDeleteError(error.response.data.message);
+    }
+  };
+
+  const projectsList = () => {
+    const data = {
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          sort: "asc",
+        },
+        {
+          label: "Name",
+          field: "name",
+          sort: "asc",
+        },
+        {
+          label: "Location",
+          field: "location",
+          sort: "asc",
+        },
+        {
+          label: "Date",
+          field: "date",
+          sort: "asc",
+        },
+        {
+          label: "Actions",
+          field: "actions",
+        },
+      ],
+      rows: [],
     };
-      
-        const deleteServiceHandler = (id) => {
-          deleteProject(id);
-        };
-      
-        return (
+
+    projects.forEach((project) => {
+      // Format the date to display only the date part
+      const parsedDate = new Date(project.date);
+      const year = parsedDate.getFullYear();
+      const month = parsedDate.getMonth() + 1; // Month is zero-indexed, so add 1
+      const day = parsedDate.getDate();
+      const formattedDate = `${year}-${month < 10 ? "0" : ""}${month}-${
+        day < 10 ? "0" : ""
+      }${day}`;
+
+      data.rows.push({
+        id: project._id,
+        name: project.name,
+        location: project.location,
+        date: formattedDate,
+        actions: (
           <Fragment>
-            <MetaData title={"All Projects"} />
-            <div className="row">
-              <div className="col-12 col-md-2">
-                <Sidebar />
-              </div>
-      
-              <div className="col-12 col-md-10">
-                <Fragment>
-                  <h1 className="my-5" style={{ color: "white", fontWeight: "bold", marginLeft:"15px"}}>All Projects</h1>
-      
-                  {loading ? (
-                    <Loader />
-                  ) : (
-                    <MDBDataTable
-                      data={projectsList()}
-                      className="px-3"
-                      bordered
-                      striped
-                      hover
-                      style={{ color: "white", fontWeight: "bold"}}
-                    />
-                  )}
-                </Fragment>
-              </div>
-            </div>
+            <Link
+              to={`/admin/portfolio/${project._id}`}
+              className="btn btn-primary py-1 px-2"
+            >
+              <i className="fa fa-pencil" title="Edit Project"></i>
+            </Link>
+            <button
+              className="btn btn-danger py-1 px-2 ml-2"
+              title="Delete Project"
+              onClick={() => deleteServiceHandler(project._id)}
+            >
+              <i className="fa fa-trash"></i>
+            </button>
           </Fragment>
-        );
-}
+        ),
+      });
+    });
+
+    return data;
+  };
+
+  const deleteServiceHandler = (id) => {
+    deleteProject(id);
+  };
+
+  return (
+    <Fragment>
+      <MetaData title={"All Projects"} />
+      <div className="row">
+        <div className="col-12 col-md-2">
+          <Sidebar />
+        </div>
+
+        <div className="col-12 col-md-10">
+          <Fragment>
+            <h1
+              className="my-5"
+              style={{ color: "white", fontWeight: "bold", marginLeft: "15px" }}
+            >
+              All Projects
+            </h1>
+
+            {loading ? (
+              <Loader />
+            ) : (
+              <MDBDataTable
+                data={projectsList()}
+                className="px-3"
+                bordered
+                striped
+                hover
+                style={{ color: "white", fontWeight: "bold" }}
+              />
+            )}
+          </Fragment>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 export default ProjectList;
