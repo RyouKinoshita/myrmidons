133312f1ee5956c077fc0b442eaa6252758edabc
RyouKinoshita <ryoukinoshita@gmail.com>
2023-11-27 11:26:34 +0800

    admin newsrvc updatepass validate

--- frontend/src/Components/User/UpdatePassword.js
+++ frontend/src/Components/User/UpdatePassword.js
@@ -1,93 +1,132 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { Fragment, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import MetaData from '../Layout/Metadata';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
-import { getToken } from '../../utils/helpers';
+import MetaData from "../Layout/Metadata";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { getToken } from "../../utils/helpers";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const UpdatePassword = () => {
+  const [error, setError] = useState("");
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [loading, setLoading] = useState(false);
+  let navigate = useNavigate();
 
-    const [oldPassword, setOldPassword] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const [isUpdated, setIsUpdated] = useState(false)
-    const [loading, setLoading] = useState(false)
-    let navigate = useNavigate();
-    const updatePassword = async (formData) => {
-        console.log(formData)
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${getToken()}`
-                }
-            }
+  const updatePassword = async (values) => {
+    setLoading(true);
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getToken()}`,
+        },
+      };
 
-            const {data } = await axios.put(`http://localhost:4001/api/v1/password/update`, formData, config)
-            setIsUpdated(data.success)
-            setLoading(false)
-            toast.success('password updated', {
-                position: toast.POSITION.BOTTOM_RIGHT
-            });
-            navigate('/me')
-        } catch (error) {
-            setError(error.response.data.message)
-        }
+      const { data } = await axios.put(
+        `http://localhost:4001/api/v1/password/update`,
+        values,
+        config
+      );
+      setIsUpdated(data.success);
+      setLoading(false);
+      toast.success("password updated", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      navigate("/me");
+    } catch (error) {
+      setError(error.response.data.message);
+      setLoading(false);
     }
-    useEffect(() => {
-        if (error) {
-            toast.error(error, {
-                position: toast.POSITION.BOTTOM_RIGHT
-            });
-        }
-    }, [error, ])
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.set('oldPassword', oldPassword);
-        formData.set('password', password);
-        updatePassword(formData)
+  useEffect(() => {
+    if (error) {
+      toast.error(error, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
     }
+  }, [error]);
 
-    return (
-        <Fragment>
-            <MetaData title={'Change Password'} />
+  return (
+    <Fragment>
+      <MetaData title={"Change Password"} />
 
-            <div className="row wrapper">
-                <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler} style={{backgroundColor:"gray"}} >
-                        <h1 className="mt-2 mb-5"  style={{color:"black",fontWeight: "bold"}}>Update Password</h1>
-                        <div className="form-group">
-                            <label htmlFor="old_password_field"  style={{color:"black",fontWeight: "bold"}}>Old Password</label>
-                            <input
-                                type="password"
-                                id="old_password_field"
-                                className="form-control"
-                                value={oldPassword}
-                                onChange={(e) => setOldPassword(e.target.value)}
-                            />
-                        </div>
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <Formik
+            initialValues={{
+              oldPassword: "",
+              password: "",
+            }}
+            validationSchema={Yup.object({
+              oldPassword: Yup.string().required("Old password is required"),
+              password: Yup.string().required("New password is required"),
+            })}
+            onSubmit={updatePassword}
+          >
+            <Form className="shadow-lg" style={{ backgroundColor: "gray" }}>
+              <h1
+                className="mt-2 mb-5"
+                style={{ color: "black", fontWeight: "bold" }}
+              >
+                Update Password
+              </h1>
+              <div className="form-group">
+                <label
+                  htmlFor="oldPassword"
+                  style={{ color: "black", fontWeight: "bold" }}
+                >
+                  Old Password
+                </label>
+                <Field
+                  type="password"
+                  id="oldPassword"
+                  name="oldPassword"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="oldPassword"
+                  component="div"
+                  className="invalid-feedback d-block"
+                />
+              </div>
 
-                        <div className="form-group">
-                            <label htmlFor="new_password_field"  style={{color:"black",fontWeight: "bold"}}>New Password</label>
-                            <input
-                                type="password"
-                                id="new_password_field"
-                                className="form-control"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
+              <div className="form-group">
+                <label
+                  htmlFor="password"
+                  style={{ color: "black", fontWeight: "bold" }}
+                >
+                  New Password
+                </label>
+                <Field
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="invalid-feedback d-block"
+                />
+              </div>
 
-                        <button type="submit" className="buttonforLogin" id="loginsbut" disabled={loading ? true : false} >Update Password</button>
-                    </form>
-                </div>
-            </div>
+              <button
+                type="submit"
+                className="buttonforLogin"
+                id="loginsbut"
+                disabled={loading ? true : false}
+              >
+                Update Password
+              </button>
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-        </Fragment>
-    )
-}
-
-export default UpdatePassword
+export default UpdatePassword;
